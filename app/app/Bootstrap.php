1f84dce47a8be33d40db9bd83faa7a375895fcf9
Tomáš Macháček <tomas.machacek@inventi.cz>
2022-10-20 12:07:25 +0200

    enable tracy

--- app/app/Bootstrap.php
+++ app/app/Bootstrap.php
@@ -14,7 +14,9 @@ class Bootstrap
 		$configurator = new Configurator;
 		$appDir = dirname(__DIR__);
 
-		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+        if (file_exists(__DIR__ . '/../config/local.neon')) {
+            $configurator->setDebugMode(TRUE);
+        }
 		$configurator->enableTracy($appDir . '/log');
 
 		$configurator->setTimeZone('Europe/Prague');
@@ -24,7 +26,7 @@ class Bootstrap
 			->addDirectory(__DIR__)
 			->register();
 
-		$configurator->addConfig($appDir . '/config/common.neon');
+		//$configurator->addConfig($appDir . '/config/common.neon');
 		$configurator->addConfig($appDir . '/config/services.neon');
 		$configurator->addConfig($appDir . '/config/local.neon');
 
