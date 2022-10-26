2b2e62fcd254b18c94fee0903ef6941cc88f0152
Tomáš Macháček <tomas.machacek@inventi.cz>
2022-10-26 12:48:51 +0200

    create config for tests

--- app/tests/bootstrap.php
+++ app/tests/bootstrap.php
@@ -12,6 +12,6 @@ $appDir = dirname(__DIR__ . '/..');
 $configurator->setTempDirectory($appDir . '/temp');
 
 $configurator->addConfig(__DIR__ . '/../config/services.neon');
-$configurator->addConfig(__DIR__ . '/../config/local.neon');
+$configurator->addConfig(__DIR__ . '/../config/test.neon');
 
 return $configurator->createContainer();
